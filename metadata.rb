03d5bd40ccc55ecf5bbeabeaca146b4c996df120
Philip Hutchins <philip@bitpay.com>
2014-09-28 10:50:38 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-insight'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures chef-insight'
+long_description 'Installs/Configures chef-insight'
+version          '0.1.0'
+
